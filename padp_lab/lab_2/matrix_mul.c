e8d506fa11fc3377aef48696dc8575d62a2ab6b6
Nishchal J <nishchaljs@gmail.com>
2022-03-21 10:07:42 +0530

    Update matrix_mul.c

--- padp_lab/lab_2/matrix_mul.c
+++ padp_lab/lab_2/matrix_mul.c
@@ -1,44 +1,35 @@
-#include<omp.h>
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-	int i=0, t=0;
-	for(t=1; t<=8; t=t*2){
-	for(i=500; i<=2000; i=i+500){
-		int r, j,l, count=0, sum=0,k;
-		r= i;
-		int** arr1 = (int**)malloc(r * sizeof(int*));
-		for(j=0;j<r;j++)
-			arr1[j]=(int*)malloc(r*sizeof(int*));
-
-		int** arr2 = (int**)malloc(r * sizeof(int*));
-		for(j=0;j<r;j++)
-			arr2[j]=(int*)malloc(r*sizeof(int*));
-
-		int** arr3 = (int**)malloc(r * sizeof(int*));
-		for(j=0;j<r;j++)
-			arr3[j]=(int*)malloc(r*sizeof(int*));
-
-		for(j=0; j<r; j++)
-			for(k=0; k<r;k++)
-				arr1[j][k] = count++;
-
-		for(j=0; j<r; j++)
-			for(k=0; k<r;k++)
-				arr2[j][k] = count++;
-
-		double x = omp_get_wtime();
-		omp_set_num_threads(t);
-		#pragma omp parallel for private(j, k)
-		for(j=0;j<r;j++)
-			for(k=0;k<r;k++)
-				for(l=0;l<r;l++)
-					arr3[j][k]+= arr1[j][l]*arr2[l][k];
-
-		double y = omp_get_wtime();
-		printf("%d Thread and %d*%d Matrix - %lf\n", t,r,r,y-x);
-
-	}
-	}
-return 0;
+#include<omp.h>
+void main(){
+	int n,t;
+	for(n=500; n<=2000; n+=500)
+		for(t=1; t<16; t*=2){
+			int **a,**b,**c;
+			a = (int**)malloc(sizeof(int*)*n);
+			b = (int**)malloc(sizeof(int*)*n);
+			c = (int**)malloc(sizeof(int*)*n);
+			int i=0,j,k;
+			for(i=0; i<n; i++){
+				a[i] = (int*)malloc(sizeof(int)*n);
+				b[i] = (int*)malloc(sizeof(int)*n);
+				c[i] = (int*)malloc(sizeof(int)*n);
+			}
+			for(i=0; i<n; i++)
+				for(j=0; j<n; j++){
+					a[i][j] = i;
+					b[i][j] = n-i;
+					c[i][j] = 0;
+				}
+			double t1 = omp_get_wtime();
+			omp_set_num_threads(t);
+			#pragma omp parallel for private(j,k) shared(a,b,c)
+			for(i=0; i<n; i++)
+				for(j=0; j<n; j++)
+					for(k=0; k<n; k++)
+						c[i][j] = a[i][k] * b[k][j];
+			double t2 = omp_get_wtime() - t1;
+			printf("N: %d Threads: %d Time: %g\n",n,t,t2);
+		}
 }
+
