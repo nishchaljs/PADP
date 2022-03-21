e5ee291dacc2f0b995b15140d8d33eb93f3c03fe
Nishchal J <nishchaljs@gmail.com>
2022-03-21 09:51:39 +0530

    Update mpirank.c

--- padp_lab/lab_5/mpirank.c
+++ padp_lab/lab_5/mpirank.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
-#include<mpi.h>
 #include<string.h>
-#include<stdlib.h>
-int main(int argc, char** argv){
-	char msg[3][20]={"HELLO","NISHCHAL","HERE"};
+#include<mpi.h>
+void main(int argc, char** argv){
+	int rank,size;
 	char temp[40];
-	int BUF_SIZE=32;
-	int root=0,tag=0,rank=0,size=0;
+	int n=32;
+	char msg[3][40]={"Hi","This is","Nishchal"};
 	MPI_Init(&argc,&argv);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	if(rank==0){
-		int i=0;
+		int i =0;
 		for(i=1; i<size; i++){
-		MPI_Status status;
-		MPI_Recv(&temp,BUF_SIZE,MPI_CHAR,i,tag,MPI_COMM_WORLD,&status);
-		printf("Msg=%s recv from %d rank to %d rank\n",temp,i,rank);
-	}
-	}
-	else{		strcpy(temp,msg[rank-1]);
-			MPI_Send(&temp,BUF_SIZE,MPI_CHAR,0,tag, MPI_COMM_WORLD);
+			MPI_Status status;
+			MPI_Recv(&temp,n,MPI_CHAR,i,0,MPI_COMM_WORLD,&status);
+			printf("%s from rank %d to rank %d\n",temp,i,rank);
 		}
+	}
+	else{
+		strcpy(temp,msg[rank-1]);
+		MPI_Send(&temp,n,MPI_CHAR,0,0,MPI_COMM_WORLD);
+	}
 	MPI_Finalize();
 }
