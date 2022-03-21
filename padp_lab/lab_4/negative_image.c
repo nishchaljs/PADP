8d7f0ca4ea1467b66708aa479af9d93c33b792f1
Nishchal J <nishchaljs@gmail.com>
2022-03-21 12:10:17 +0530

    Update negative_image.c

--- padp_lab/lab_4/negative_image.c
+++ padp_lab/lab_4/negative_image.c
@@ -1,66 +1,54 @@
 #include<stdio.h>
-#include<gd.h>
-#include<error.h>
 #include<omp.h>
+#include<gd.h>
 #include<stdlib.h>
-int main(int argc, char** argv){
-	if(argc!=3){
-		error(1,0,"Invalid args. Try ./a.out inp.png out.png");
-		return 0;
+void main(int argc, char** argv){
+	if(argc<3){
+		printf("Enter ./a.out inp.png op.png\n");
+		return;
 	}
-	else{
-		FILE *inp, *op;
-		if((inp = fopen(argv[1],"r"))==NULL){
-			error(1,0,"INP FILE CANNOT BE OPENED");
-			return 0;
-		}
-		else{int t;
-		     gdImagePtr img;
-		     img = gdImageCreateFromPng(inp);
-			for(t=1; t<16; t*=2){
-			int w = gdImageSX(img);
-			int h = gdImageSY(img);
-			int i=0,j;
-			double t1 = omp_get_wtime();
-			omp_set_num_threads(t);
-			#pragma omp parallel for private(i,j) shared(img)
-			for(i=0; i<w; i++)
-				for(j=0; j<h; j++)
-				{
-					int tid = omp_get_thread_num();
-					int color = gdImageGetPixel(img,i,j);
-					int red = gdImageRed(img,color);
-					int green = gdImageGreen(img, color);
-					int blue = gdImageBlue(img, color);
-					int temp = (red+green+blue)/3;
-					red=green=blue=temp;
-					if(tid==0){
-						color = gdImageColorAllocate(img,red,green,blue);
-						gdImageSetPixel(img,i,j,color);
-						}
-					if(tid==1){
-						color = gdImageColorAllocate(img,red,green,blue);
-						gdImageSetPixel(img,i,j,color);
-						}
-					if(tid==2){
-						color = gdImageColorAllocate(img,red,green,blue);
-						gdImageSetPixel(img,i,j,color);
-						}
-					if(tid==3){
-						color = gdImageColorAllocate(img,red,green,blue);
-						gdImageSetPixel(img,i,j,color);
-						}
-					
-				}
-			double t2 = omp_get_wtime() -t1;
-			printf("The time taken is %g\n", t2);
+	FILE* fp;
+        fp = fopen(argv[1], "r");
+	if(fp==NULL){
+		printf("FILE NO SUPPORTED\n");
+		return;
+	}
+	gdImagePtr image;
+	image = gdImageCreateFromPng(fp);
+	int w = gdImageSX(image);
+	int h = gdImageSY(image);
+	int i,j;
+	//printf("HEYYY\n");
+	omp_set_num_threads(3);
+	double t1 = omp_get_wtime();
+#pragma omp parallel for private(j) shared(image) schedule(dynamic,10) //static, dynamic, guided
+	for(i=0; i<w; i++)
+		for(j=0; j<h; j++){
+			int tid = omp_get_thread_num();
+			int color = gdImageGetPixel(image,i,j);
+			int red = gdImageRed(image,color);
+			int green = gdImageGreen(image,color);
+			int blue = gdImageBlue(image,color);
+			int temp = (red+green+blue)/3;
+			red = green = blue = temp;
+			if(tid==0){
+				color = gdImageColorAllocate(image,red,0,0);
+			        gdImageSetPixel(image,i,j,color);
 			}
-			op = fopen(argv[2],"w");
-			gdImagePng(img,op);
-			gdImageDestroy(img);
-
-		
-	}
-	}
-	return 0;
+			if(tid==1){
+				color = gdImageColorAllocate(image,0,green,0);
+			        gdImageSetPixel(image,i,j,color);
+			}
+			if(tid==2){
+				color = gdImageColorAllocate(image,0,0,blue);
+			        gdImageSetPixel(image,i,j,color);
+			}
+		}
+	double t2 = omp_get_wtime() - t1;
+	printf("Time:%g\n",t2);
+	FILE* fp1 = fopen(argv[2], "w");
+	gdImagePng(image,fp1);
+	//fclose(fp1);
+	gdImageDestroy(image);
+	//fclose(fp);
 }
