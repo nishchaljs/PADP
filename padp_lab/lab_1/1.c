cd107e8073c7d17e8dfcecb773f8ed5940b3fbab
Nishchal J <nishchaljs@gmail.com>
2022-03-21 09:33:24 +0530

    Update 1.c

--- padp_lab/lab_1/1.c
+++ padp_lab/lab_1/1.c
@@ -1,18 +1,24 @@
-#include <omp.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
+void main(){
+	long n; int t, seed = 35792468;
+	for(n=10000; n<100000000; n*=10)
+		for(t=1; t<16; t*=2){
+			double pi; long i; int count = 0;
+			double t1 = omp_get_wtime();
+			omp_set_num_threads(t);
+			#pragma omp parallel for reduction(+:count)
+			for(i=0; i<n; i++){
+				double x = (double)rand_r(&seed)/RAND_MAX;
+				double y = (double)rand_r(&seed)/RAND_MAX;
+				double z = x*x + y*y;
+				if(z<=1)
+					count++;
+			}
+			pi = (double)count/n*4;
+			double t2 = omp_get_wtime()-t1;
+			printf("Iter: %ld, Threads: %d, Pi: %g, Time: %g\n",n,t,pi,t2);
 
-int main(int argc, char* argv[])
-{
-	int nthreads, tid;
-	#pragma omp parallel private(nthreads, tid)
-	{
-		tid = omp_get_thread_num();
-		printf("Welcome to GFG from thread = %d\n",tid);
-
-		if (tid == 0) {
-			nthreads = omp_get_num_threads();
-			printf("Number of threads = %d\n",nthreads);
 		}
-	}
 }
